ace2011707a9c4769e0406050ed1098a35ca4b35
rifacoder <jstrfaheem065@gmail.com>
2020-07-08 03:49:09 +0530

    Auth middleware,

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const router = express.Router();
 
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
+
 // @route GET api/auth
 // @desc test route
 // @acces public
-router.get("/", (req, res, next) => {});
+router.get("/", auth, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal server error.");
+  }
+});
 
 module.exports = router;
