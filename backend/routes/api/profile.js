21db851fca85b9145db6b452a4db62f0a65ba7ec
rifacoder <jstrfaheem065@gmail.com>
2020-07-09 19:35:32 +0530

    Delete profile init,

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -145,4 +145,22 @@ router.get("/user/:user_id", async (req, res, next) => {
   }
 });
 
+// @route DELETE api/profile
+// @desc Delete profile, user & posts
+// @acces Private
+
+router.delete("/", auth, async (req, res, next) => {
+  try {
+    // @todo - remove users posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted." });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
