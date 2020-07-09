e1b2790b3e39dc5c570ae6095a5f6b4e0cde961a
rifacoder <jstrfaheem065@gmail.com>
2020-07-10 00:55:04 +0530

    Adding experiences,

--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -163,4 +163,57 @@ router.delete("/", auth, async (req, res, next) => {
   }
 });
 
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @acces Private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required.").not().isEmpty(),
+      check("company", "Company is required.").not().isEmpty(),
+      check("from", "From date is required.").not().isEmpty(),
+    ],
+  ],
+  async (req, res, next) => {
+    const erors = validationResult(req);
+    if (!erors.isEmpty()) {
+      return res.status(400).json({ erors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+      await profile.save();
+      return res.json(profile);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal server error.");
+    }
+  }
+);
+
 module.exports = router;
