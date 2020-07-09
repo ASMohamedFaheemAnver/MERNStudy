340aa82162fe76e3c1f67c9cbfa6759eee1324bc
rifacoder <jstrfaheem065@gmail.com>
2020-07-10 03:25:33 +0530

    Create post,

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -2,9 +2,45 @@ const express = require("express");
 
 const router = express.Router();
 
-// @route GET api/posts
-// @desc test route
-// @acces public
-router.get("/", (req, res, next) => {});
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route POST api/posts
+// @desc Create post
+// @acces Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is requied.").not().isEmpty()]],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await (await User.findById(req.user.id)).isSelected(
+        "-password"
+      );
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      return res.json(post);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal server error.");
+    }
+  }
+);
 
 module.exports = router;
