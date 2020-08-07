467f156a049b0d3aa9205db8734328ad920ae21b
rifacoder <jstrfaheem065@gmail.com>
2020-08-07 17:04:27 +0530

    Profile actions and reducers,

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,6 +3,8 @@ import {
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
 } from "../actions/types";
 
 const initalState = {
@@ -20,10 +22,22 @@ export default function (state = initalState, action) {
     case GET_PROFILE:
     case UPDATE_PROFILE:
       return { ...state, profile: payload, loading: false };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
     case PROFILE_ERROR:
       return { ...state, error: payload, loading: false };
     case CLEAR_PROFILE:
       return { ...state, profile: null, repos: [], loading: false, error: {} };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false,
+      };
     default:
       return state;
   }
